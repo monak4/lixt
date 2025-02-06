4d04439b86451936474084fc5bb9c78341fc5b6b
monak4 <monakada@gmail.com>
2025-02-06 22:20:30 +0900

    first step

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"image/color"
 	"log"
 	"os"
 
 	"gioui.org/app"
 	"gioui.org/op"
-	"gioui.org/text"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
@@ -25,29 +24,22 @@ func main() {
 
 func run(window *app.Window) error {
 	theme := material.NewTheme()
+	editor := &widget.Editor{
+		SingleLine: false,
+		Submit:     false,
+		ReadOnly:   false,
+	}
 	var ops op.Ops
+
 	for {
 		switch e := window.Event().(type) {
 		case app.DestroyEvent:
 			return e.Err
 		case app.FrameEvent:
-			// This graphics context is used for managing the rendering state.
 			gtx := app.NewContext(&ops, e)
 
-			// Define an large label with an appropriate text:
-			title := material.H1(theme, "Hello, Gio")
+			material.Editor(theme, editor, "Hello, World!").Layout(gtx)
 
-			// Change the color of the label.
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-
-			// Change the position of the label.
-			title.Alignment = text.Middle
-
-			// Draw the label to the graphics context.
-			title.Layout(gtx)
-
-			// Pass the drawing operations to the GPU.
 			e.Frame(gtx.Ops)
 		}
 	}
