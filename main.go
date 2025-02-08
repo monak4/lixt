4578137ae6c160398d0fb69e6a9cb8ee49892fcb
monak4 <monakada@gmail.com>
2025-02-08 19:48:49 +0900

    editor side scrollable

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/paint"
+	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+)
+
+var (
+	white  = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
+	white_ = color.NRGBA{R: 255, G: 255, B: 255, A: 80}
 )
 
 func main() {
@@ -32,6 +38,7 @@ func run(window *app.Window) error {
 		SingleLine: false,
 		Submit:     false,
 		ReadOnly:   false,
+		WrapPolicy: text.WrapWords,
 	}
 	backgroundColor := color.NRGBA{
 		R: 30,
@@ -40,8 +47,9 @@ func run(window *app.Window) error {
 		A: 255,
 	}
 
+	var list widget.List
+	list.Axis = layout.Horizontal
 	var ops op.Ops
-
 	for {
 		switch e := window.Event().(type) {
 		case app.DestroyEvent:
@@ -55,12 +63,14 @@ func run(window *app.Window) error {
 			layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
 					ed := material.Editor(theme, editor, "Type here...")
-					ed.Color = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
-					ed.HintColor = color.NRGBA{R: 255, G: 255, B: 255, A: 80}
-					return layout.UniformInset(unit.Dp(8)).Layout(gtx, ed.Layout)
+					ed.Color = white
+					ed.HintColor = white_
+
+					return material.List(theme, &list).Layout(gtx, 1, func(gtx layout.Context, index int) layout.Dimensions {
+						return layout.UniformInset(unit.Dp(8)).Layout(gtx, ed.Layout)
+					})
 				}),
 			)
-
 			e.Frame(gtx.Ops)
 		}
 	}
